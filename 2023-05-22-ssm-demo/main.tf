fa3c68f28a81630068923b6fa616da630a77dc4c
atoonk <andree@toonk.nl>
2023-05-23 07:55:18 -0700

    Adding connector

--- 2023-05-22-ssm-demo/main.tf
+++ 2023-05-22-ssm-demo/main.tf
@@ -5,7 +5,7 @@ variable "region" {
 
 variable "instance_count" {
   description = "Number of instances to create"
-  default     = 3
+  default     = 1
 }
 
 
@@ -27,6 +27,7 @@ resource "aws_internet_gateway" "main" {
 resource "aws_subnet" "public" {
   vpc_id     = aws_vpc.main.id
   cidr_block = "10.0.1.0/24"
+  map_public_ip_on_launch = true
   tags = {
     Name = "public_subnet"
   }
@@ -138,6 +139,110 @@ resource "aws_instance" "main" {
   iam_instance_profile = aws_iam_instance_profile.ssm_profile.name
   
   tags = {
-    Name = "ssm-border0-demo-${count.index}"
+    Name = "ssm-border0-demo-${count.index}",
+    border0 = "group=infra_team,type=ssh"
+    
+  }
+}
+
+# Now we'll create the connector.  It will be the ssm client 
+# and needs ssm client iam permissions
+
+# Firstly, create a new IAM role for the second EC2 instance:
+
+resource "aws_iam_role" "connector_role" {
+  name = "border0_connector_role"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "ec2.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+}
+
+#Then, create an IAM policy for the required permissions:
+
+resource "aws_iam_policy" "connector_policy" {
+  name = "border0_connector_policy"
+  description = "Policy for SSM client permissions"
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Action": [
+        "ssm:StartSession",
+        "ssm:TerminateSession",
+        "ec2:Describe*",
+        "iam:SimulatePrincipalPolicy"
+
+      ],
+      "Resource": "*"
+    }
+  ]
+}
+EOF
+}
+
+#Then, attach the policy to the role:
+
+resource "aws_iam_role_policy_attachment" "connector_policy_attachment" {
+  role       = aws_iam_role.connector_role.name
+  policy_arn = aws_iam_policy.connector_policy.arn
+}
+
+# Create an instance profile for the role:
+
+resource "aws_iam_instance_profile" "connector_profile" {
+  name = "b0_connector_profile"
+  role = aws_iam_role.connector_role.name
+}
+
+
+resource "aws_instance" "ssm_client" {
+  ami           = data.aws_ami.ubuntu-image.id  
+  instance_type = "t2.micro"
+  subnet_id     = aws_subnet.public.id
+  iam_instance_profile = aws_iam_instance_profile.connector_profile.name
+  associate_public_ip_address = true
+  vpc_security_group_ids      = [aws_security_group.allow_ssh.id]
+  tags = {
+    Name = "ssm-border0-connector"
+  }
+}
+
+resource "aws_security_group" "allow_ssh" {
+  name        = "allow_ssh"
+  description = "Allow SSH inbound traffic"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allow_ssh"
   }
 }
